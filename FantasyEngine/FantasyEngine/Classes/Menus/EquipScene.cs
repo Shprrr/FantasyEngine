bfbf62adf4ac1ceb61eaa46d214266d68c8d9ad5
Maxime Talon <maximetalon@hotmail.com>
2012-12-06 02:11:42 +0000

    Ménage oublié
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection]$/FantasyEngine/Src;C37

--- FantasyEngine/FantasyEngine/Classes/Menus/EquipScene.cs
+++ FantasyEngine/FantasyEngine/Classes/Menus/EquipScene.cs
@@ -11,19 +11,19 @@ namespace FantasyEngine.Classes.Menus
         private readonly Color COLOR_NORMAL = Color.White;
         private readonly Color COLOR_UNEQUIP = Color.Gray;
 
-        private Cursor CursorWindow;
+        private Cursor _CursorWindow;
         private Window _EquipWindow;
         /// <summary>
         /// Character who is shown.
         /// </summary>
-        public Character ActiveCharacter;
+        public Character CurrentActor;
 
         public EquipScene(Game game, Character activeCharacter)
             : base(game)
         {
-            CursorWindow = new Cursor(Game, 6, 2);
+            _CursorWindow = new Cursor(Game, 6, 2);
             _EquipWindow = new Window(Game, 76, 55, 488, 145);
-            ActiveCharacter = activeCharacter;
+            CurrentActor = activeCharacter;
         }
 
         public override void DrawGUI(GameTime gameTime)
@@ -35,53 +35,53 @@ namespace FantasyEngine.Classes.Menus
             spriteBatchGUI.Scissor(_EquipWindow.InsideBound);
 
             spriteBatchGUI.DrawString(GameMain.font, "Equipment", new Vector2(260, 68) + spriteBatchGUI.CameraOffset, Color.White);
-            spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.Name, new Vector2(90, 96) + spriteBatchGUI.CameraOffset, Color.White);
+            spriteBatchGUI.DrawString(GameMain.font, CurrentActor.Name, new Vector2(90, 96) + spriteBatchGUI.CameraOffset, Color.White);
 
-            if (ActiveCharacter.RightHand != null)
-                spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.RightHand.Name,
+            if (CurrentActor.RightHand != null)
+                spriteBatchGUI.DrawString(GameMain.font, CurrentActor.RightHand.Name,
                     new Vector2(110, 126) + spriteBatchGUI.CameraOffset, COLOR_NORMAL);
             else
                 spriteBatchGUI.DrawString(GameMain.font, "Right Hand",
                     new Vector2(110, 126) + spriteBatchGUI.CameraOffset, COLOR_UNEQUIP);
 
-            if (ActiveCharacter.LeftHand != null)
-                spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.LeftHand.Name,
+            if (CurrentActor.LeftHand != null)
+                spriteBatchGUI.DrawString(GameMain.font, CurrentActor.LeftHand.Name,
                     new Vector2(110, 148) + spriteBatchGUI.CameraOffset, COLOR_NORMAL);
             else
                 spriteBatchGUI.DrawString(GameMain.font, "Left Hand",
                     new Vector2(110, 148) + spriteBatchGUI.CameraOffset, COLOR_UNEQUIP);
 
-            if (ActiveCharacter.Head != null)
-                spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.Head.Name,
+            if (CurrentActor.Head != null)
+                spriteBatchGUI.DrawString(GameMain.font, CurrentActor.Head.Name,
                     new Vector2(347, 126) + spriteBatchGUI.CameraOffset, COLOR_NORMAL);
             else
                 spriteBatchGUI.DrawString(GameMain.font, "Head",
                     new Vector2(347, 126) + spriteBatchGUI.CameraOffset, COLOR_UNEQUIP);
 
-            if (ActiveCharacter.Body != null)
-                spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.Body.Name,
+            if (CurrentActor.Body != null)
+                spriteBatchGUI.DrawString(GameMain.font, CurrentActor.Body.Name,
                     new Vector2(347, 148) + spriteBatchGUI.CameraOffset, COLOR_NORMAL);
             else
                 spriteBatchGUI.DrawString(GameMain.font, "Body",
                     new Vector2(347, 148) + spriteBatchGUI.CameraOffset, COLOR_UNEQUIP);
 
-            if (ActiveCharacter.Arms != null)
-                spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.Arms.Name,
+            if (CurrentActor.Arms != null)
+                spriteBatchGUI.DrawString(GameMain.font, CurrentActor.Arms.Name,
                     new Vector2(110, 170) + spriteBatchGUI.CameraOffset, COLOR_NORMAL);
             else
                 spriteBatchGUI.DrawString(GameMain.font, "Arms",
                     new Vector2(110, 170) + spriteBatchGUI.CameraOffset, COLOR_UNEQUIP);
 
-            if (ActiveCharacter.Feet != null)
-                spriteBatchGUI.DrawString(GameMain.font, ActiveCharacter.Feet.Name,
+            if (CurrentActor.Feet != null)
+                spriteBatchGUI.DrawString(GameMain.font, CurrentActor.Feet.Name,
                     new Vector2(347, 170) + spriteBatchGUI.CameraOffset, COLOR_NORMAL);
             else
                 spriteBatchGUI.DrawString(GameMain.font, "Feet",
                     new Vector2(347, 170) + spriteBatchGUI.CameraOffset, COLOR_UNEQUIP);
 
             // Draw cursor
-            CursorWindow.Position = new Vector2((CursorWindow.CursorIndex % 2 == 0 ? 90 : 327), 126 + CursorWindow.CursorIndex / 2 * 22);
-            CursorWindow.Draw(gameTime);
+            _CursorWindow.Position = new Vector2((_CursorWindow.CursorIndex % 2 == 0 ? 90 : 327), 126 + _CursorWindow.CursorIndex / 2 * 22);
+            _CursorWindow.Draw(gameTime);
 
             spriteBatchGUI.ScissorReset();
         }
@@ -90,63 +90,44 @@ namespace FantasyEngine.Classes.Menus
         {
             base.Update(gameTime);
 
-            Input.UpdateInput(gameTime);
-
             if (!Input.UpdateInput(gameTime))
                 return;
 
-            CursorWindow.Update(gameTime);
+            _CursorWindow.Update(gameTime);
 
             if (Input.keyStateDown.IsKeyDown(Keys.Enter))
             {
-                BaseItem lastItemEquiped = null;
-                switch (CursorWindow.CursorIndex)
+                switch (_CursorWindow.CursorIndex)
                 {
                     case 0: // Right Hand
-                        lastItemEquiped = Player.GamePlayer.Actors[0].RightHand;
-                        Player.GamePlayer.Actors[0].RightHand = null;
+                        CurrentActor.Unequip(Character.eEquipSlot.RightHand, Player.GamePlayer.Inventory);
                         break;
 
                     case 1: // Head
-                        lastItemEquiped = Player.GamePlayer.Actors[0].Head;
-                        Player.GamePlayer.Actors[0].Head = null;
+                        CurrentActor.Unequip(Character.eEquipSlot.Head, Player.GamePlayer.Inventory);
                         break;
 
                     case 2: // Left Hand
-                        lastItemEquiped = Player.GamePlayer.Actors[0].LeftHand;
-                        Player.GamePlayer.Actors[0].LeftHand = null;
+                        CurrentActor.Unequip(Character.eEquipSlot.LeftHand, Player.GamePlayer.Inventory);
                         break;
 
                     case 3: // Body
-                        lastItemEquiped = Player.GamePlayer.Actors[0].Body;
-                        Player.GamePlayer.Actors[0].Body = null;
+                        CurrentActor.Unequip(Character.eEquipSlot.Body, Player.GamePlayer.Inventory);
                         break;
 
                     case 4: // Arms
-                        lastItemEquiped = Player.GamePlayer.Actors[0].Arms;
-                        Player.GamePlayer.Actors[0].Arms = null;
+                        CurrentActor.Unequip(Character.eEquipSlot.Arms, Player.GamePlayer.Inventory);
                         break;
 
                     case 5: // Feet
-                        lastItemEquiped = Player.GamePlayer.Actors[0].Feet;
-                        Player.GamePlayer.Actors[0].Feet = null;
+                        CurrentActor.Unequip(Character.eEquipSlot.Feet, Player.GamePlayer.Inventory);
                         break;
                 }
-
-                if (lastItemEquiped != null)
-                    Player.GamePlayer.Inventory.Add(lastItemEquiped);
             }
 
             if (Input.keyStateDown.IsKeyDown(Keys.Escape)
                 || Input.keyStateDown.IsKeyDown(Keys.Back))
             {
-                //if (_CursorSortBeginIndex >= 0)
-                // {
-                //     _CursorIndex = _CursorSortBeginIndex;
-                //     _CursorSortBeginIndex = -1;
-                //     _UseCommand.Enabled = true;
-                // }
-                // else
                 Scene.RemoveSubScene();
             }
 
